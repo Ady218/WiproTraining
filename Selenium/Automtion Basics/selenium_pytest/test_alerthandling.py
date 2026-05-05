5866efa608c168b2dbb8516d4bc905b8e85c0222
Ady218 <arpitdas.603abc@gmail.com>
2026-05-06 00:37:47 +0530

    sel_pytest

---
+++ Selenium/Automtion Basics/selenium_pytest/test_alerthandling.py
@@ -0,0 +1,58 @@
+import time
+
+import pytest
+from pytest_check import check
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope='module')
+def driver():
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get('https://the-internet.herokuapp.com/javascript_alerts')
+    yield driver
+    driver.quit()
+
+def test_simple_js_alert(driver):
+    driver.find_element(By.XPATH, "//button[text()='Click for JS Alert']").click()
+    alert = driver.switch_to.alert
+    assert alert.text == "I am a JS Alert", "Alert text was wrong"
+    time.sleep(3)
+    alert.accept()
+    time.sleep(3)
+    result = driver.find_element(By.ID, "result").text
+    assert result == "You successfully clicked an alert", "Result text was wrong"
+
+def test_js_confirmdismiss(driver):
+    driver.find_element(By.XPATH, "//button[text()='Click for JS Confirm']").click()
+    alert = driver.switch_to.alert
+    assert alert.text == "I am a JS Confirm", "Alert text was wrong"
+    time.sleep(3)
+    alert.dismiss()
+    time.sleep(3)
+    result = driver.find_element(By.ID, "result").text
+    assert result == "You clicked: Cancel", "Result text was wrong"
+
+
+def test_js_confirmok(driver):
+    driver.find_element(By.XPATH, "//button[text()='Click for JS Confirm']").click()
+    alert = driver.switch_to.alert
+    assert alert.text == "I am a JS Confirm", "Alert text was wrong"
+    time.sleep(3)
+    alert.accept()
+    time.sleep(3)
+    result = driver.find_element(By.ID, "result").text
+    assert result == "You clicked: Ok", "Result text was wrong"
+
+def test_js_prompt(driver):
+    driver.find_element(By.XPATH, "//button[text()='Click for JS Prompt']").click()
+    alert = driver.switch_to.alert
+    time.sleep(3)
+    assert alert.text == "I am a JS prompt", "Alert text was wrong"
+    alert.send_keys("Python Selenium")
+    time.sleep(3)
+    alert.accept()
+    time.sleep(3)
+    result = driver.find_element(By.ID, "result").text
+    assert result == "You entered: Python Selenium", "Result text was wrong"
---
+++ Selenium/Automtion Basics/selenium_pytest/test_amazon.py
@@ -0,0 +1,54 @@
+import time
+from os import waitpid
+
+import pytest
+from pytest_check import check
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium_basics.waits import search_box
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get('https://www.amazon.in')
+    yield driver
+    driver.quit()
+def test_open_amazon(driver):
+    assert "amazon" in driver.current_url, 'Url for amazon is not correct'
+    assert "Amazon" in driver.title, "Title for amazon is not correct"
+    print("\nOpened Amazon Homepage. Title & URL verified")
+
+def test_search_product(driver):
+    wait = WebDriverWait(driver, 5)
+    search_box = wait.until(EC.presence_of_element_located((By.ID, 'twotabsearchtextbox')))
+    search_box.clear()
+    search_box.send_keys("wireless mouse")
+
+    search_button = driver.find_element(By.ID, 'nav-search-submit-button')
+    search_button.click()
+    # assert "wireless" in driver.current_url
+    assert driver.current_url.__contains__('wireless'), 'Search results page did not load'
+    assert driver.title.__contains__('wireless'), 'Search result did not load the page'
+    print("\nSearch results page loaded successfully")
+
+def test_find_elements_amazon(driver):
+    wait = WebDriverWait(driver, 15)
+
+    # Single product title
+    first_product = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a h2 span")))
+    print("\nFirst Prod: ", first_product.text)
+
+    product_titles = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a h2 span")))
+
+    print(f"\nFound{len(product_titles)} product titles on page one.\n")
+
+    for i, title in enumerate(product_titles[:5], start=1):
+        print(f"{i}. {title.text}")
+
+    assert len(product_titles) > 0, "No products found on Amazon search results"
